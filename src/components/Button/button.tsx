e192fdd77fbfcd3ad9aa381d28db3cbcaab3b2eb
lrh21g <837233792@qq.com>
2020-07-05 23:15:33 +0800

    更正

--- src/components/Button/button.tsx
+++ src/components/Button/button.tsx
@@ -3,7 +3,7 @@ import classNames from "classnames";
 
 /**
  * TypeScript知识点：
- * Union Types（联合类型）：表示一个值可以是几种类型之一。 用竖线（ |）分隔每个类型
+ * 字符串字面量类型:允许指定字符串必须的固定值
  */
 export type ButtonSize = "lg" | "sm";
 export type ButtonType = "primary" | "default" | "danger" | "link";
@@ -19,6 +19,7 @@ interface BaseButtonProps {
 
 /**
  * TypeScript知识点：
+ * Union Types（联合类型）：表示一个值可以是几种类型之一。 用竖线（ |）分隔每个类型
  * Intersection Types（交叉类型）：将多个类型合并为一个类型。它包含了所需的所有类型的特性。用（ &）符号合并每个类型
  * Partial：将一个已知的类型每个属性都变为可选的
  * 
